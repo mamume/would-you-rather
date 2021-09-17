32dff4042e19c1bfffce9885e13285f771a8755e
mamume <mmustafametwally@gmail.com>
2021-09-17 17:53:17 +0200

    Leaderboard Created

--- would-you-rather/src/components/UserScoreCard.js
+++ would-you-rather/src/components/UserScoreCard.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box';
 
 
